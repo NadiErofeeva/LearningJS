09d5681b12cd005b4262173030bfd862df173ab2
Nadi Erofeeva <n.erofeeva72@gmail.com>
2023-01-30 21:48:34 +0500

    Закончен урок 03 сложный уровень

--- js/hard.js
+++ js/hard.js
@@ -1,3 +1,5 @@
+"use strict";
+/*
 let num = 266219; // Создать переменную num со значением 266219
 
 const arr = Array.from(String(num), Number);
@@ -9,3 +11,45 @@ console.log(result);
 
 console.log(String(result).slice(0,2)); // Вывести в консоль первые 2 цифры полученного числа
 
+*/
+
+
+let lang = 'ru';
+const weekdayRu = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
+const weekdayEn = 'Monday, Tuesday, Wednesday, Thursday, Saturday, Sunday';
+
+const weekday = {
+    'ru': weekdayRu,
+    'en': weekdayEn
+};
+
+if (lang === 'ru') {
+    console.log(weekdayRu);
+} else if (lang === 'en') {
+    console.log(weekdayEn);
+} else {
+    console.log('Нет значений');
+}
+
+switch (lang) {
+    case 'ru':
+        console.log(weekdayRu);
+        break;
+    case 'en':
+        console.log(weekdayEn);
+        break;
+    default:
+        console.log('Нет значений');
+}
+
+console.log(weekday[lang] || 'Нет значений');
+
+/// Задание 2
+
+let namePerson = 'Артем';
+
+const result = namePerson === 'Артем' ?
+    console.log('Директор') :
+    namePerson === 'Александр' ?
+        console.log('Преподаватель') :
+        console.log('Студент');
