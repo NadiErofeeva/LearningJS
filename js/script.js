ec7e7ee9b34b17d3781a1a9ed4c3d82e392a9fcc
Nadi Erofeeva <n.erofeeva72@gmail.com>
2023-02-02 18:50:18 +0500

    Закончен урок 05 легкий уровень

--- js/script.js
+++ js/script.js
@@ -1,16 +1,61 @@
 'use strict';
 
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать?');
-const screenPrice = +prompt('Сколько будет стоить данная работа?');
-const adaptive = confirm('Нужен ли адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = +prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = +prompt('Сколько это будет стоить?');
-const rollback = 20;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.round(fullPrice - (fullPrice * (rollback / 100)));
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let rollback = 20;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+let service1;
+let service2;
+
+
+const isNumber = function (num) {
+    return !isNaN(num) && isFinite(num);
+};
+
+const asking = function () {
+    title = prompt('Как называется ваш проект?', 'Проект');
+    screens = prompt('Какие типы экранов нужно разработать?', 'много, сложно');
+
+   /* screenPrice = +prompt('Сколько будет стоить данная работа?');
+
+    while (!isNumber(screenPrice)) {
+        screenPrice = prompt('Сколько будет стоить данная работа?');
+    }*/
+
+    do {
+        screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?'));
+    } while (!isNumber(screenPrice));
+
+    adaptive = confirm('Нужен ли адаптив на сайте?');
+};
+
+
+const getAllServicePrices = function () {
+    let sum = 0;
+
+    for(let i = 0; i < 2; i++) {
+
+        if (i === 0) {
+            service1 = prompt('Какой дополнительный тип услуги нужен?', 'первый');
+        } else if (i === 1) {
+            service2 = prompt('Какой дополнительный тип услуги нужен?', 'Второй');
+        }
+
+        sum += parseFloat(prompt('Сколько это будет стоить?'));
+
+        while (!isNumber(sum)) {
+            sum = parseFloat(prompt('Сколько это будет стоить?'));
+        }
+
+    }
+    return sum;
+    // return servicePrice1 + servicePrice2;
+};
+
 
 const showTypeOf = function (variable) {
     console.log(variable, typeof variable);
@@ -29,21 +74,13 @@ const getRollbackMessage = function (price) {
 };
 
 
-const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2;
-};
-const allServicePrices = getAllServicePrices();
-
-
 function getFullPrice() {
     return screenPrice + allServicePrices;
 }
-fullPrice = getFullPrice();
 
 
 const getTitle = function () {
-    const newTitle = title.trim().toLowerCase();
-    return newTitle[0].toUpperCase() + newTitle.substring(1);
+    return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase();
 };
 
 
@@ -51,14 +88,28 @@ const getServicePercentPrices = function () {
     return getFullPrice() - (getFullPrice() * (rollback / 100));
 };
 
+
+
+asking();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrices();
-
-
+title = getTitle();
 
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
 
-console.log('вывод строки с типами экранов ', screens);
+console.log('allServicePrices', allServicePrices);
+
+console.log(getRollbackMessage(fullPrice));
+
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log('вывод строки с типами экранов ', screens.length);
 console.log('сообщение о скидке пользователю ', getRollbackMessage(fullPrice));
 console.log('стоимость за вычетом процента отката посреднику ', servicePercentPrice);
+console.log('стоимость верстки экранов ' + screenPrice + ' рублей и Стоимость разработки сайта ' + fullPrice);
+
